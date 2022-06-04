69f44a825eb9f328d40e65971d5662d9081f9186
eshevvy <eshevvy@gmail.com>
2022-06-04 04:11:16 -0700

    test 2

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - prints the sizes of various types the computer is allowed to run
- * Return: gives a return value of 0 when main is executed successfully 
+ * Return: gives a return value of 0 when main is executed successfully
  * otherwise, non-zero
  * char c - denotes single character
  * int i - denotes interger
@@ -15,11 +15,11 @@ int main(void)
 	long long int lli;
 	float f;
 
-	printf("Size of a char is: %ld byte(s)\n", sizeof(char));
-	printf("Size of an int is: %ld byte(s)\n", sizeof(int));
-	printf("Size of a long int is: %ld byte(s)\n", sizeof(long int));
-	printf("Size of a long long int is: %ld byte(s)\n", sizeof(long
+	printf("Size of a char: %ld byte(s)\n", sizeof(char));
+	printf("Size of an int: %ld byte(s)\n", sizeof(int));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(long
 			long int));
-	printf("Size of a float is: %ld byte(s)\n", sizeof(float));
+	printf("Size of a float: %ld byte(s)\n", sizeof(float));
 	return (0);
 }
