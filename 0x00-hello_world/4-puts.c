879f95ea3bc0318fd1d375ad7e2279c137a693e0
eshevvy <eshevvy@gmail.com>
2022-06-04 01:24:45 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * main - A function that prints the phrase "Programming is like building
+ * main:A function that prints the phrase "Programming is like building
  * a multilingual puzzle which is followed by a new line"
- * return - It gives a value 0 if main is executed properly and 
+ * return:It gives a value 0 if main is executed properly and 
  * non zero otherwise.
  */
 int main(void)
