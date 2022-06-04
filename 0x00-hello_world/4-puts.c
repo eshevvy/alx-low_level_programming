3cb88d2066793397f24eb9494e8f09080fa75694
eshevvy <eshevvy@gmail.com>
2022-06-04 01:46:36 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 /**
  * main - a funtion that prints the phrase "Programming is like building a
  * multilingual puzzle"
- * return - 0 if exited properly, non zero otherwise
+ * Return: 0 if exited properly, non zero otherwise
  */
 int main(void)
 {	puts("\"Programming is like building a multilingual puzzle");
