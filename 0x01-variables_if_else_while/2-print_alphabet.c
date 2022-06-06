3ff368346edaae9cd5cb2e42c890cb7647e75151
eshevvy <eshevvy@gmail.com>
2022-06-06 16:01:47 -0700

    I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,10 +4,17 @@
  * Return: gives a value of zero, if functions main executes successfully,
  * non-zero otherwise.
  */
+# define char  a /* first lower case letter of the alphabet */
+# define z    25 /* last lower case letter of the alphabet */
+# define step  1 /* step size */
 int main(void)
 {
-	int c;
+	int char;
 
-	while ((c = getchar()) != EOF)
-		putchar(c);
+	for (char =a; a <= z; a == a+step)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
 }
