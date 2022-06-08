65244353ab79bf2176a7a89f9f567e21b1d2e4f1
eshevvy <eshevvy@gmail.com>
2022-06-08 13:30:25 -0700

    test

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,20 +1,19 @@
-# include "main.h"
+# include <stdio.h>
 /**
  * main - function that prints the alphabets in lower cas, followed by 
  * a new line.
  * Return: gives a return value of 0 when main executes successfully.
  */
-int main(void)
 void print_alphabet(void)
 {
 	char ch;
 
 	ch = 'a';
+
 	while (ch <= 'z')
 	{
-		_putchar(ch);
+		putchar(ch);
 		ch++;
 	}
-	_putchar('\n');
-	return (0);
+	putchar('\n');
 }
