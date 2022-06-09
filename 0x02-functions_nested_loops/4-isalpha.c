851f147a65742b861baa485ea5256f84ddbf5a3b
eshevvy <eshevvy@gmail.com>
2022-06-09 08:41:47 -0700

    4. isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,29 @@
+# include "main.h"
+
+/**
+ * _isalpha - checks for alphabetic character
+ * @c: the character that checks for lower
+ * or upper case.
+ * Return: gives a value of 1 is check is
+ * successful else, 0
+ */
+int _isalpha(int c)
+{
+	int i, j;
+
+	i = 'a';
+
+	while (i >= 'a' && i <= 'z')
+	{
+		_putchar(i);
+		i++;
+		if (j > 0)
+		{
+			(j >= 'A' && j <= 'Z');
+			_putchar(j);
+			return (1);
+		}
+		else
+			return (0);
+	}
+}
