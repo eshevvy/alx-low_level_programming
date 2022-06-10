254f7f417edf6da4e910f4e3b26d5f22317c978a
eshevvy <eshevvy@gmail.com>
2022-06-10 12:05:59 -0700

    hot fix

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,10 +15,8 @@ void times_table(void)
 		for (j = 0; j < 10; j++)
 		{
 			k = i * j;
-			if (j == 0)
-			{
-				_putchar(k + '0');
-			}
+			_putchar(k + '0');
+
 			if (k < 10 && j != 0)
 			{
 				_putchar(',');
