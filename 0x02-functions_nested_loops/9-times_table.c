bea75196f7edc0252012b088cc6a46b148ccf7c8
eshevvy <eshevvy@gmail.com>
2022-06-10 11:53:54 -0700

    9. Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,32 +10,30 @@ void times_table(void)
 {
 	int i, j, k;
 
-	i = 0;
-
-	while (i <= 9)
+	for (i = 0; i < 10; i++)
 	{
-		j = 0;
-
-		k = (i * j);
-		while (j <= 9)
+		for (j = 0; j < 10; j++)
 		{
-			_putchar(k + '0');
-			j++;
+			k = i * j;
+			if (j == 0)
+			{
+				_putchar(k + '0');
+			}
+			if (k < 10 && j != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(k + '0');
+			}
+			else if (k >= 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
+			}
 		}
-		if (k < 10 && j >= 0)
-		{
-			_putchar(',');
-			_putchar(' ');
-			_putchar(' ');
-			_putchar(k + '0');
-		}
-		else if (k >= 10)
-		{
-			_putchar(',');
-			_putchar(' ');
-			_putchar((k / 10) + '0');
-			_putchar((k % 10) + '0');
-		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
