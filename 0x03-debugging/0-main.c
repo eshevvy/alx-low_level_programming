6fb25b1d56a1b970e5be150115f4aae36a9b7d71
eshevvy <eshevvy@gmail.com>
2022-06-13 08:41:06 -0700

    debugging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,24 +1,15 @@
-# include <stdio.h>
-# include <stdlib.h>
-# include <time.h>
+# include "main.h"
 
 /**
- * main - will assign a number to the variable n each time it's executed.
- * Return: this will give a value of 0 if the main function is executed
- * succesfully, else non-zero value.
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
  */
 
 int main(void)
 {
-	int n;
+	int i;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if (n > 0)
-		printf("%d is positive\n", n);
-	else if (n == 0)
-		printf("%d is zero\n", n);
-	else if (n < 0)
-		printf("%d is negative\n", n);
+	i = 0;
+	positive_or_negative(i);
 	return (0);
 }
