7971dfc6a525865f8e50430064403e09b4516f58
eshevvy <eshevvy@gmail.com>
2022-06-13 07:47:49 -0700

    task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,24 @@
+# include <stdio.h>
+# include <stdlib.h>
+# include <time.h>
+
+/**
+ * main - will assign a number to the variable n each time it's executed.
+ * Return: this will give a value of 0 if the main function is executed
+ * succesfully, else non-zero value.
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n == 0)
+		 printf("%d is zero\n", n);
+	else if (n < 0)
+		printf("%d is negative\n", n);
+	return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,3 @@
+# 0. Multiple mains. "tests function that prints if integer is positive or negative"
+# 1. Like, comment, subscribe. "main - causes an infinite loop"
+# 3. Leap year. "main - takes a date and prints how many days are left in the year, taking leap years into account"
