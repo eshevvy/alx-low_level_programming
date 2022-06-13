eba5f48698786693b216fefe56ccec6145dcf7ef
eshevvy <eshevvy@gmail.com>
2022-06-13 11:51:04 -0700

    debugging

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -5,6 +5,7 @@
  * @a: first integer
  * @b: second integer
  * @c: third integer
+ * int largest - largest number
  * Return: largest number
  */
 
@@ -15,17 +16,14 @@ int largest_number(int a, int b, int c)
 	if (a > b && a > c)
 	{
 		largest = a;
-		printf("%d is the largest number\n", largest);
 	}
 	else if (b > a && b > c)
 	{
 		largest = b;
-		printf("%d is the largest number\n", largest);
 	}
 	else
 	{
 		largest = c;
-		printf("%d is the largest number\n", largest);
 	}
 
 	return (largest);
