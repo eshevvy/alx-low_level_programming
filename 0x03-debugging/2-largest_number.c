b305ec17a8144d48c2c602bd30dacda388f02775
eshevvy <eshevvy@gmail.com>
2022-06-13 10:41:30 -0700

    hot fix

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -36,13 +36,13 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && b > c)
+	if ((a > b) && b > c)
 	{
 		largest = a;
 
 		printf("%d is the largest number\n", largest);
 	}
-	else if (b > a && a > c)
+	else if ((b > a) && a > c)
 	{
 		largest = b;
 
