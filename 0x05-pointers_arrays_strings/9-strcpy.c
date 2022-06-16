a5542fff701c13640fd6fc925d0f3eb4edd71fca
eshevvy <eshevvy@gmail.com>
2022-06-16 03:08:35 -0700

    9. strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -10,10 +10,9 @@
  * Return value: the pointer to dest.
  */
 
-char *
 char *_strcpy(char *dest, char *src)
 {
-	size_t i, n;
+	int i, n;
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[i] = src[i];
