279321a574cc52d6f051be78fa1fcca15e1b3c1a
eshevvy <eshevvy@gmail.com>
2022-06-16 18:39:00 -0700

    2. strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -5,7 +5,7 @@
  * @dest: destination string
  * @src: source string
  * @n: number of bytes to copy
- * 
+ *
  * Return: pointer to the resulting string
  */
 char *_strncpy(char *dest, char *src, int n)
