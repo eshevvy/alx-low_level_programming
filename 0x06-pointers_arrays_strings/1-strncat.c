bc77f654d2a192e3d73747c3a35898d50eefec7a
eshevvy <eshevvy@gmail.com>
2022-06-16 18:08:33 -0700

    1. strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,26 +1,34 @@
-#include <main.h>
+#include "main.h"
 #include <string.h>
 
 /**
- * *_strncat - function that concatenates two strings.
+ * *_strcat - function that concatenates two strings. It appends the src
+ * string to the dest string, overwriting the terminating null byte (\0) at
+ * the end of dest, and adds a terminating null byte.
  *
- * @dest: string that src appends to.
- * @src: appended string, and does not need to be null
- * terminated if it contains n or more bytes.
- * @n: number of bytes.
+ * @src: string to be appended to the dest string.
+ * @dest: string whose null byte will be overwritten.
+ * @n: the number of bytes to concatenate.
  *
- * Return: returns pointer to dest.
+ * Return: returns pointer to dest string.
  */
 
-char *_strncat(char *dest, char *src, int n)
+char *_strcat(char *dest, char *src int n)
 {
-	size_t dest_len = strlen(dest);
-	size_t i;
+	size_t i, j;
 
-	for (i = 0; src[i] != '\0'; i++)
-		dest[dest_len + i] = src[i];
-	for (j = 0; j < i; j++)
-		dest[dest_len + i] = '\0';
+	i = 0;
+	j = 0;
+
+	while (dest[i] != '\0')
+		i++;
+	while (src[n] != '\0' && j < n)
+	{
+		dest[i] = src[n];
+		j++;
+		i++;
+	}
+	dest[i] = '\0';
 
 	return (dest);
 }
